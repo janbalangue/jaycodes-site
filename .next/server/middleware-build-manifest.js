6aada0c8611b953e8c79e43a5ec12f51c9137eda
Jan Balangue <jan.a.balangue@gmail.com>
2025-05-21 18:11:20 -0700

    New scaffolding

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,30 +1,26 @@
-self.__BUILD_MANIFEST = {
-  polyfillFiles: ["static/chunks/polyfills.js"],
-  devFiles: ["static/chunks/react-refresh.js"],
-  ampDevFiles: [],
-  lowPriorityFiles: [],
-  rootMainFiles: [],
-  pages: {
-    "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js",
-    ],
-    "/_app": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/_app.js",
-    ],
-    "/_error": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/_error.js",
-    ],
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
   },
-  ampFirstPages: [],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [
+    "static/chunks/node_modules_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_fd44f5a4._.js",
+    "static/chunks/node_modules_next_dist_compiled_2ce9398a._.js",
+    "static/chunks/node_modules_next_dist_client_8f19e6fb._.js",
+    "static/chunks/node_modules_next_dist_2ecbf5fa._.js",
+    "static/chunks/node_modules_@swc_helpers_cjs_00636ac3._.js",
+    "static/chunks/_e69f0d32._.js",
+    "static/chunks/_93808211._.js"
+  ],
+  "ampFirstPages": []
 };
-self.__BUILD_MANIFEST.lowPriorityFiles = [
-  "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-  ,
-  "/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
 ];
